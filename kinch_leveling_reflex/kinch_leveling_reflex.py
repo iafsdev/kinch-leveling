14cca1bce3856c8eefbeb6a6cb0b25e55a5da0be
iafsdev <iafs.0232@gmail.com>
2025-07-17 11:49:53 -0600

    Update: Mueve el contenido de index del archivo principal a su propio archivo page

--- kinch_leveling_reflex/kinch_leveling_reflex.py
+++ kinch_leveling_reflex/kinch_leveling_reflex.py
@@ -1,48 +1,8 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
-from kinch_leveling_reflex.pages.table import table
-from kinch_leveling_reflex.pages.modal import modal
+from kinch_leveling_reflex.pages.index import index
 from kinch_leveling_reflex.api.api import fastapi_app
 from rxconfig import config
-from kinch_leveling_reflex.states.TableState import TableState
-from kinch_leveling_reflex.states.HeaderState import HeaderState
-from kinch_leveling_reflex.states.WCAState import WCAState
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.flex(
-        rx.container(   
-            rx.heading(
-            'Kinch Leveling',
-            size='8',
-            font_weight='bold',
-            align='center',
-            ),
-        ),
-        rx.flex(
-            rx.text(f'PB Kinch: {HeaderState.pb_kinch}'),
-            rx.text(f'PR Kinch: {HeaderState.pr_kinch}'),
-            rx.text(f'NR Kinch: {HeaderState.nr_kinch}'),
-            rx.text(f'WR Kinch: {HeaderState.wr_kinch}'),
-            rx.text(f'XP: {HeaderState.xp_total}'),
-            justify='center',
-            spacing='9',
-        ),
-        table(),
-        modal(),
-        direction='column',
-        align='center',
-        spacing='3',
-    )
 
 app = rx.App(
     api_transformer=fastapi_app,
 )
-app.add_page(index, on_load=[TableState.load_data, HeaderState.load_data])
---
+++ kinch_leveling_reflex/pages/index.py
@@ -0,0 +1,33 @@
+import reflex as rx
+from kinch_leveling_reflex.components.table import table
+from kinch_leveling_reflex.components.modal import modal
+from kinch_leveling_reflex.states.TableState import TableState
+from kinch_leveling_reflex.states.HeaderState import HeaderState
+
+@rx.page("/", title="Kinch Leveling", on_load=[TableState.load_data, HeaderState.load_data])
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.flex(
+        rx.container(   
+            rx.heading(
+            'Kinch Leveling',
+            size='8',
+            font_weight='bold',
+            align='center',
+            ),
+        ),
+        rx.flex(
+            rx.text(f'PB Kinch: {HeaderState.pb_kinch}'),
+            rx.text(f'PR Kinch: {HeaderState.pr_kinch}'),
+            rx.text(f'NR Kinch: {HeaderState.nr_kinch}'),
+            rx.text(f'WR Kinch: {HeaderState.wr_kinch}'),
+            rx.text(f'XP: {HeaderState.xp_total}'),
+            justify='center',
+            spacing='9',
+        ),
+        table(),
+        modal(),
+        direction='column',
+        align='center',
+        spacing='3',
+    )
